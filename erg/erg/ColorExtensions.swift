822dda3074540ea9aaa5147d53eadcda19db7141
Christie Davis <christiedavis22@gmail.com>
2018-02-09 09:18:09 +1300

    Add colour rgb extension methods

--- erg/erg/ColorExtensions.swift
+++ erg/erg/ColorExtensions.swift
@@ -7,9 +7,32 @@
 //
 
 import Foundation
+import UIKit
 //32292F
 //99E1D9
 //F0F7F4
 //70ABAF
 //8A7A74
 
+extension UIColor {
+
+    static var darkBrown: UIColor {
+        return UIColor(red: 50.0, green: 41.0, blue: 47.0, alpha: 1.0)
+    }
+    
+    static var lightTeal: UIColor {
+        return UIColor(red: 153.0, green: 225.0, blue: 217.0, alpha: 1.0)
+    }
+    
+    static var tealWhite: UIColor {
+        return UIColor(red: 240.0, green: 247.0, blue: 244.0, alpha: 1.0)
+    }
+    
+    static var darkTeal: UIColor {
+        return UIColor(red: 112.0, green: 171.0, blue: 175.0, alpha: 1.0)
+    }
+    
+    static var lightBrown: UIColor {
+        return UIColor(red: 138.0, green: 122.0, blue: 116.0, alpha: 1.0)
+    }
+}

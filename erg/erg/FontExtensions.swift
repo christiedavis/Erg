dba3ac5bc3ce153d84376274b325d9d2f8120aad
Christie Davis <christiedavis22@gmail.com>
2018-02-09 10:00:13 +1300

    Add font extensions

---
+++ erg/erg/FontExtensions.swift
@@ -0,0 +1,33 @@
+//
+//  FontExtensiuons.swift
+//  erg
+//
+//  Created by Christie Davis on 9/02/18.
+//  Copyright © 2018 star. All rights reserved.
+//
+
+import UIKit
+
+let curlyFontName: String = "Sofia-Regular"
+let boldFontName: String = "Lora-Bold"
+let regularFontName: String = "Lora-Regular"
+let italicFontName: String = "Lora-Italic"
+
+extension UIFont {
+    
+    static func curlyFont(_ size: Int = 25) -> UIFont {
+        return UIFont(name: curlyFontName, size: CGFloat(size))!
+    }
+    
+    static func boldFont(_ size: Int = 12) -> UIFont {
+        return UIFont(name: curlyFontName, size: CGFloat(size))!
+    }
+    
+    static func regularFont(_ size: Int = 12) -> UIFont {
+        return UIFont(name: curlyFontName, size: CGFloat(size))!
+    }
+    
+    static func curlyFontLarge(_ size: Int = 12) -> UIFont {
+        return UIFont(name: curlyFontName, size: CGFloat(size))!
+    }
+}

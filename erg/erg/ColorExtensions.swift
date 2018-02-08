f6c2161ecbf863a1bc2bcf4f670369624dff0d21
Christie Davis <christiedavis22@gmail.com>
2018-02-09 00:12:57 +1300

    Add colour extension file

---
+++ erg/erg/ColorExtensions.swift
@@ -0,0 +1,15 @@
+//
+//  ColorExtensions.swift
+//  erg
+//
+//  Created by Christie Davis on 9/02/18.
+//  Copyright © 2018 star. All rights reserved.
+//
+
+import Foundation
+//32292F
+//99E1D9
+//F0F7F4
+//70ABAF
+//8A7A74
+

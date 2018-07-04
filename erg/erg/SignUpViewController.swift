220ab7f58adfa6f75236e0ee4789d8f99c8fddab
christie <christiedavis22@gmail.com>
2018-07-05 10:31:15 +1200

    Update signup view

--- erg/erg/SignUpViewController.swift
+++ erg/erg/SignUpViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FirebaseAuth
 import YXWaveView
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: BaseViewController {
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -19,6 +19,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var createAccountButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.showLoading()
         
         emailField.textContentType = .username
         passwordField.textContentType = .password
@@ -29,12 +30,16 @@ class SignUpViewController: UIViewController {
         createAccountButton.layer.cornerRadius = 5
 
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+        self.dismissLoading()
     }
     
     @IBAction func didTapSignUp(_ sender: UIButton) {
+        self.showLoading()
         let email = emailField.text
         let password = passwordField.text
         Auth.auth().createUser(withEmail: email!, password: password!, completion: { (user, error) in
+            self.dismissLoading()
+            
             if let error = error {
                 if let errCode = AuthErrorCode(rawValue: error._code) {
                     switch errCode {
